322ba785ebd4ede44dba35b45ae0ad65eab06f58
yassinecher <yassinecherni9@gmail.com>
2025-02-27 11:48:02 +0100

    up

--- src/main/java/com/batigobackend/batigo/Repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.batigobackend.batigo.Repository;
-
- import com.batigobackend.batigo.Entity.Expense;
-  import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository  extends JpaRepository<Expense, Integer> {
-}
